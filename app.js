d6e9a7284ead3d96a0e82017993065163df5ef5a
Amdrii <andreyk557@gmail.com>
2023-03-04 14:32:57 +0200

    feat[func / price]

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const squareInput = document.querySelector('#square-input')
 const squareRange = document.querySelector('#square-range')
 const inputs = document.querySelectorAll('input')
-
+const totalPriseAlemant = document.querySelector('#total-price')
+const radioTypes =document.querySelectorAll('input[name="type"]')
 
 
 const basePrice = 6000;
 
-
-
+console.log(radioTypes)
+console.log(totalPriseAlemant)
 console.log(inputs)
 console.log(squareInput)
 console.log(squareRange)
@@ -23,7 +24,12 @@ squareRange.addEventListener('input', function() {
 
 function calcPrice () {
     let totalPrice = basePrice * parseInt(squareInput.value)
-    console.log(totalPrice)
+    console.log(totalPrice);
+
+
+    
+    const formater = new Intl.NumberFormat('ru')
+    totalPriseAlemant.innerText = formater.format(totalPrice);
 }
 
 calcPrice()
